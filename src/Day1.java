b7ab6f143149400a6b87338ccc705ffa425534d8
dhicks6 <dhicks23@msudenver.edu>
2022-12-09 09:32:39 -0700

    Currently printing out the highest signle calorie amount

--- src/Day1.java
+++ src/Day1.java
@@ -17,10 +17,24 @@ public class Day1 {
             10000
                 """;
     
-    for (String s : calories.split("/n")) {
-        System.out.println(s);
+        int calorie = 0;
+        int mostCalories = 0;
+
+        for (String s : calories.split("\n")) {
+            String strCal = s.trim();
+
+            if (strCal.isBlank()) {
+                if (mostCalories < calorie) {
+                    mostCalories = calorie;
+                }
+                calorie = 0;
+                continue;
+            }
+            calorie = Integer.parseInt(strCal);
     }
-    
-    
+    if (mostCalories < calorie) {
+        mostCalories = calorie;
+    }
+    System.out.println(mostCalories);
     }
 }
