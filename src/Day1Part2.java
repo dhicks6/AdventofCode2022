6bfeb0fd2205b5f53a891bdb4a3f2a5b91ed3028
dhicks6 <dhicks23@msudenver.edu>
2022-12-09 09:46:36 -0700

    Renamed day 1 to Day1Part1 and made a day 1 part 2

---
+++ src/Day1Part2.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Day1Part2 {
+    public static void main(String[] args) throws Exception{
+        String calories = """
+            1000
+            2000
+            3000
+            
+            4000
+            
+            5000
+            6000
+            
+            7000
+            8000
+            9000
+
+            10000
+                """;
+    
+        BufferedReader br = new BufferedReader(new FileReader("inputs/day1.txt"));
+        
+        int calorie = 0;
+        int mostCalories = 0;
+        String line;
+
+        //for (String line : calories.split("\n")) {
+        while ((line = br.readLine()) != null) {
+            String strCal = line.trim();
+
+            if (strCal.isBlank()) {
+                if (mostCalories < calorie) {
+                    mostCalories = calorie;
+                }
+                calorie = 0;
+                continue;
+            }
+            calorie += Integer.parseInt(strCal);
+    }
+    if (mostCalories < calorie) {
+        mostCalories = calorie;
+    }
+    System.out.println(mostCalories);
+    }
+}
