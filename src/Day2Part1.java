1b2be0facf21986baebc04e1c579ff9d1350b530
dhicks6 <dhicks23@msudenver.edu>
2022-12-11 12:37:47 -0700

    Downlaoded day 2 input file and started setting up day 2 part 1

---
+++ src/Day2Part1.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Day2Part1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        /*
+         Scores are +1 point for rock, +2 for paper, +3 for scissors, +0 for a loss, +3 for a draw, and +6 for a win
+         */
+        BufferedReader br = new BufferedReader(new FileReader("inputs/InputDay2.txt"));
+        final int rock = 1;
+        final int paper = 2;
+        final int scissors = 3;
+        final int draw = 3;
+        final int win = 6;
+        int totalScore;
+
+        
+    }
+}
